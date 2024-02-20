3e96c9560e516d1340399a3aca793f1521df9871
abbas <abbasmomeny1994@gmail.com>
2024-02-20 12:51:44 +0330

    add: New()

--- repository/mysql/db.go
+++ repository/mysql/db.go
@@ -1,4 +1,27 @@
 package mysql
 
-type DB struct {
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
+)
+
+// ...
+
+type MYSQLDB struct {
+	db *sql.DB
+}
+
+func New() *MYSQLDB {
+	db, err := sql.Open("mysql", "root:password@/gameapp_db")
+	if err != nil {
+		panic(fmt.Errorf("unable to open mysql : %v", err))
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return &MYSQLDB{db: db}
 }
