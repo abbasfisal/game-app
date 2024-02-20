9ce60677f5de7ba9af016d0a2054513a61bb2aed
abbas <abbasmomeny1994@gmail.com>
2024-02-20 07:20:33 +0330

    add : entities

--- entity/game.go
+++ entity/game.go
@@ -4,6 +4,19 @@ type Game struct {
 	ID          uint
 	CategoryID  uint
 	QuestionIDs []uint
-	PlayerIDs   []uint
-	WinnerID    uint
+	Players     []Player
+}
+type Player struct {
+	ID      uint
+	UserID  uint
+	GameID  uint
+	Score   uint
+	Answers []PlayerAnswer
+}
+
+type PlayerAnswer struct {
+	ID         uint
+	PlayerID   uint
+	QuestionID uint
+	Choice     PossibleAnswerChoice
 }
