273b58bfd3a1bd6363c34dd88ef99dfe8b9ce38b
abbas <abbasmomeny1994@gmail.com>
2024-02-20 15:47:01 +0330

    change timestamp to datetime

--- repository/mysql/setup_db.sql
+++ repository/mysql/setup_db.sql
@@ -2,5 +2,5 @@ create table users(
     id int primary key  AUTO_INCREMENT,
     name varchar(255) not null ,
     phone_number varchar(255) not null unique ,
-    created_at timestamp default current_timestamp
+    created_at datetime default current_timestamp
 )
