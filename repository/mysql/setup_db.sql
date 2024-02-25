f075a9669935c158e09679b5ee364565fad1614d
abbas <abbasmomeny1994@gmail.com>
2024-02-25 08:30:07 +0330

    add: col password

--- repository/mysql/setup_db.sql
+++ repository/mysql/setup_db.sql
@@ -1,6 +1,8 @@
-create table users(
-    id int primary key  AUTO_INCREMENT,
-    name varchar(255) not null ,
-    phone_number varchar(255) not null unique ,
-    created_at datetime default current_timestamp
+create table users
+(
+    id           int primary key AUTO_INCREMENT,
+    name         varchar(255) not null,
+    phone_number varchar(255) not null unique,
+    password     varchar(255) not null,
+    created_at   datetime default current_timestamp
 )
