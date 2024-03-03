1140d32986fd35804439445bb59c7a67651d2d2d
abbas <abbasmomeny1994@gmail.com>
2024-03-03 16:56:59 +0330

    add: dto layer

---
+++ deliver/dto/user_register.go
@@ -0,0 +1,13 @@
+package dto
+
+import "github.com/abbasfisal/game-app/entity"
+
+type RegisterRequest struct {
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phone_number"`
+	Password    string `json:"password"`
+}
+
+type RegisterResponse struct {
+	User entity.User
+}
--- service/userservice/service.go
+++ service/userservice/service.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"github.com/abbasfisal/game-app/deliver/dto"
 	"github.com/abbasfisal/game-app/entity"
 	"github.com/abbasfisal/game-app/pkg/phonenumber"
 	"github.com/abbasfisal/game-app/pkg/richerror"
@@ -39,39 +40,23 @@ func New(authGenerator AuthGenerator, repo Repository) Service {
 	}
 }
 
-// RegisterRequest -- Request
-type RegisterRequest struct {
-	Name        string `json:"name"`
-	PhoneNumber string `json:"phone_number"`
-	Password    string `json:"password"`
-}
-
-// RegisterResponse -- Response
-type RegisterResponse struct {
-	User entity.User
-}
-
-//var (
-//	PhoneNumberIsNotValid = errors.New("phone number is not valid")
-//)
-
-func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
+func (s Service) Register(req dto.RegisterRequest) (dto.RegisterResponse, error) {
 	//validate phone
 	if !phonenumber.IsValid(req.PhoneNumber) {
-		return RegisterResponse{}, fmt.Errorf("phone number is not valid")
+		return dto.RegisterResponse{}, fmt.Errorf("phone number is not valid")
 	}
 
 	//check uniqueness phone
 	isUnique, err := s.repo.IsPhoneNumberUnique(req.PhoneNumber)
 	if err != nil {
-		return RegisterResponse{}, fmt.Errorf("unexpected error %w", err)
+		return dto.RegisterResponse{}, fmt.Errorf("unexpected error %w", err)
 	}
 	if !isUnique {
-		return RegisterResponse{}, fmt.Errorf("phone number is not unique")
+		return dto.RegisterResponse{}, fmt.Errorf("phone number is not unique")
 	}
 	//validate name
 	if len(req.Name) <= 3 {
-		return RegisterResponse{}, fmt.Errorf("name must be greater than 3")
+		return dto.RegisterResponse{}, fmt.Errorf("name must be greater than 3")
 	}
 
 	//store user into db
@@ -83,11 +68,11 @@ func (s Service) Register(req RegisterRequest) (RegisterResponse, error) {
 	}
 	createdUser, err := s.repo.Register(u)
 	if err != nil {
-		return RegisterResponse{}, fmt.Errorf("unexpected eror %w", err)
+		return dto.RegisterResponse{}, fmt.Errorf("unexpected eror %w", err)
 	}
 
 	//return use
-	return RegisterResponse{createdUser}, nil
+	return dto.RegisterResponse{User: createdUser}, nil
 }
 
 func GetMD5Hash(text string) string {
@@ -95,54 +80,36 @@ func GetMD5Hash(text string) string {
 	return hex.EncodeToString(hash[:])
 }
 
-type LoginRequest struct {
-	PhoneNumber string `json:"phone_number"`
-	Password    string `json:"password"`
-}
-type Tokens struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-}
-type UserInfo struct {
-	ID          uint   `json:"id"`
-	PhoneNumber string `json:"phone_number"`
-	Name        string `json:"name"`
-}
-type LoginResponse struct {
-	User   UserInfo `json:"user"`
-	Tokens Tokens   `json:"tokens"`
-}
-
-func (s Service) Login(req LoginRequest) (LoginResponse, error) {
+func (s Service) Login(req dto.LoginRequest) (dto.LoginResponse, error) {
 	const op = "userservice.Login"
 	user, exist, err := s.repo.GetUserByPhoneNumber(req.PhoneNumber)
 
 	if err != nil {
-		return LoginResponse{}, richerror.New(op).WithError(err)
+		return dto.LoginResponse{}, richerror.New(op).WithError(err)
 
 	}
 
 	if !exist {
-		return LoginResponse{}, fmt.Errorf("username or password isnt correct! ")
+		return dto.LoginResponse{}, fmt.Errorf("username or password isnt correct! ")
 	}
 	if user.Password != GetMD5Hash(req.Password) {
-		return LoginResponse{}, fmt.Errorf("username or password isnt correct! ")
+		return dto.LoginResponse{}, fmt.Errorf("username or password isnt correct! ")
 	}
 
 	accessToken, err := s.auth.CreateAccessToken(user)
 	if err != nil {
-		return LoginResponse{}, fmt.Errorf("unexpected error while generating jwt : %w", err)
+		return dto.LoginResponse{}, fmt.Errorf("unexpected error while generating jwt : %w", err)
 	}
 
 	refreshToken, err := s.auth.CreateRefreshToken(user)
 
-	return LoginResponse{
-		User: UserInfo{
+	return dto.LoginResponse{
+		User: dto.UserInfo{
 			ID:          user.ID,
 			PhoneNumber: user.PhoneNumber,
 			Name:        user.Name,
 		},
-		Tokens: Tokens{
+		Tokens: dto.Tokens{
 			AccessToken:  accessToken,
 			RefreshToken: refreshToken,
 		},
