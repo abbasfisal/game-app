30c12f95ebbe3b96d904d16b73144a89c0026f00
abbas <abbasmomeny1994@gmail.com>
2024-03-04 06:20:45 +0330

    add: entities related to authorizations

---
+++ entity/access_control.go
@@ -0,0 +1,16 @@
+package entity
+
+// AccessControl :access controller only keeps allowed permission
+type AccessControl struct {
+	ID           uint
+	ActorID      uint
+	ActorType    ActorType
+	PermissionID uint
+}
+
+type ActorType string
+
+const (
+	RoleActorType = "role"
+	UserActorType = "user"
+)
--- repository/mysql/migrations/1709061605_add_password_column_to_users_table.sql
+++ repository/mysql/migrations/1709061605_add_password_column_to_users_table.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE users
-    ADD COLUMN password varchar(255) not null;
+    ADD COLUMN password varchar(191) not null;
 
 -- +migrate Down
 ALTER TABLE users DROP COLUMN password;
